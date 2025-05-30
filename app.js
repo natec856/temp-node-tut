3117331809604d05b72f2497615173584155a100
Nate Colavolpe <nate.colavolpe@gmail.com>
2025-05-30 16:09:50 -0400

    added methods.public files

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const app = express()
+let {people} = require('./data')
 
+
+
+
+app.get('/api/people', (req, res) => {
+    res.status(200).json({success:true,data:people})
+})
 
 
 app.listen(5000, ()=> {
