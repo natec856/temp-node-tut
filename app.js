e8e441c4fdf9ff51ed2cd50b9dae366cefa43ad0
Nate Colavolpe <nate.colavolpe@gmail.com>
2025-05-30 21:07:16 -0400

    routes

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
-let {people} = require('./data')
+const people = require('./routes/people')
+const auth = require('./routes/auth')
 
+// static assets
+app.use(express.static('./2-express-tutorial/methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
+app.use('/api/people', people)
 
+app.use('/login', auth)
 
-app.get('/api/people', (req, res) => {
-    res.status(200).json({success:true,data:people})
-})
-
-
-app.listen(5000, ()=> {
-    console.log('Server is listening on port 5000...')
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
 })
